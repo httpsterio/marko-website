7331ebb16bcb24c3c71ff16ef0f4d50ea59ad791
Sami Mäkelä <sami@httpster.io>
2023-06-03 01:42:41 +0300

    add screen size and custom color palettes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     require('@tailwindcss/typography')
   ],
   theme: {
+    screens: {
+      'xs': '400px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         'arctic': {
